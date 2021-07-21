b9d5c5329e158b1f7e9d38c31a97a8156b46d52c
15310 <1335968147@qq.com>
2021-07-21 13:59:28 +0800

    1-5 服务端实现——反射调用

---
+++ rpc-core/src/main/java/server/RpcServer.java
@@ -0,0 +1,46 @@
+package server;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.*;
+
+/**
+ * 远程方法调用的提供者（服务端）
+ * 使用一个ServerSocket监听某个端口，循环接收连接请求，如果发来了请求就创建一个线程，在新线程中处理调用。
+ * 这里创建线程采用线程池
+ * Created by tangssst@qq.com on 2021/07/21
+ */
+public class RpcServer {
+
+    private final ExecutorService threadPool;
+    private static final Logger logger = LoggerFactory.getLogger(RpcServer.class);
+
+    public RpcServer() {
+        int corePoolSize = 5;
+        int maximumPoolSize = 50;
+        long keepAliveTime = 60;
+        BlockingQueue<Runnable> workingQueue = new ArrayBlockingQueue<>(100);
+        ThreadFactory threadFactory = Executors.defaultThreadFactory();
+        threadPool = new ThreadPoolExecutor(corePoolSize, maximumPoolSize, keepAliveTime, TimeUnit.SECONDS, workingQueue, threadFactory);
+    }
+
+    /**
+     * 这里向工作线程WorkerThread传入了socket和用于服务端实例service。
+     */
+    public void register(Object service, int port) {
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            logger.info("服务器正在启动...");
+            Socket socket;
+            while((socket = serverSocket.accept()) != null) {
+                logger.info("客户端连接！Ip为：" + socket.getInetAddress());
+                threadPool.execute(new WorkerThread(socket, service));
+            }
+        } catch (IOException e) {
+            logger.error("连接时有错误发生：", e);
+        }
+    }
+
+}
