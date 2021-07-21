8d4c82766bf469cc775a30090f3bee18a82e555b
15310 <1335968147@qq.com>
2021-07-21 09:08:05 +0800

    1-2.1 修复错误

--- rpc-api/src/main/java/api/HelloObject.java
+++ rpc-api/src/main/java/api/HelloObject.java
@@ -3,13 +3,15 @@ package api;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.io.Serializable;
+
 /**
  * HelloService的vo对象
  * Created by tangssst@qq.com on 2021/07/21
  */
 @Data
 @AllArgsConstructor
-public class HelloObject {
+public class HelloObject implements Serializable {
     private Integer id;
     private String message;
 }
